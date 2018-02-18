d5ecc4c531961b7dbee9b317fe01da0d27e0d248
Muhammad Atta khan <m.atta.khan@hotmail.com>
2018-02-19 00:57:59 +0500

    code

--- cachematrix.R
+++ cachematrix.R
@@ -1,77 +1,34 @@
-pollutantmean <- function(directory , pollutant = 'sulfate' , id = 1:100){
-  
-  if(grep("specdata", directory) == 1) {
-    directory <- ("C:/Users/muhammad.atta/Documents/specdata/")
-  }
-  
-  all_file <-  as.character(list.files(directory))
+## Put comments here that give an overall description of what your
+## functions do
 
-  file_Paths <- paste(directory,all_file , sep="")
-  mean_vector = c()
+## Write a short comment describing this function
 
-  for (i in id) {
-    
-    con <- file(file_Paths[i],'r')
-    file_read <- read.csv(con,header = T , sep = ",")
-    head(file_read)
-    na_removed <- file_read[!is.na(file_read[,pollutant]),pollutant]
-    mean_vector <- c(mean_vector,na_removed)
-    on.exit(close(con))
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
   }
-  
-  result <- mean(mean_vector)
-  
-  return(round(result,3))
-  
+  get <- function() x
+  setmetric <- function(solve) m <<- solve
+  getmetric <- function() m
+  list(set = set, get = get,
+       setmetric = setmetric,
+       getmetric = getmetric)
 }
 
-complete <- function(directory  , id = 1:100){
-  
-  if(grep("specdata", directory) == 1) {
-    directory <- ("C:/Users/muhammad.atta/Documents/specdata/")
-  }
-  
-  all_file <-  as.character(list.files(directory))
-  
-  file_Paths <- paste(directory,all_file , sep="")
-  nobs = numeric()
-  
-  for (i in id) {
-    
-    con <- file(file_Paths[i],'r')
-    file_read <- read.csv(con,header = T , sep = ",")
-    head(file_read)
-    nobs = c(nobs,sum(complete.cases(file_read)))
-    on.exit(close(con))
-  }
-  
-  result <- data.frame(id,nobs)
-  
-  return(result)
-  
-}
 
-corr <- function(directory, threshold = 0) {
-  if(grep("specdata", directory) == 1) {
-    directory <- ("C:/Users/muhammad.atta/Documents/specdata/")
-  }
-  
-  all_file <-  as.character(list.files(directory))
-  
-  file_Paths <- paste(directory,all_file , sep="")
-  nobs = numeric()
-  
-    tcorr <- function(fname){
-      data <- read.csv(file.path(directory, fname))
-      nobs <- sum(complete.cases(data))
-      if (nobs > threshold) {
-        return (cor(data$nitrate, data$sulfate, use="complete.obs"))
-      }
-      
-    }
+## Write a short comment describing this function
 
-    tcorrs <- sapply(list.files(directory), tcorr) #get all correlations + NULLs
-    tcorrs <- unlist(tcorrs[!sapply(tcorrs, is.null)]) #remove NULLs
-    return (tcorrs)
-    
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getmetric()
+  if(!is.null(m)) {
+    message("getting cached metric")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmetric(m)
+  m
 }
